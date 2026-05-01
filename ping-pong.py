6f795eb0408442064f0892410b4edd40420456e9
gazan <149593530+qwerty1234567890098765432@users.noreply.github.com>
2026-05-01 19:02:06 +0300

    Update ping-pong.py
    всё реализовано

--- ping-pong.py
+++ ping-pong.py
@@ -1,10 +1,12 @@
 import pygame as pg
 pg.init()
-
+bounces_count = 0
 window = pg.display.set_mode((800, 500))
 bg = (38, 123, 163)
 window.fill(bg)
 game = True
+font1 = pg.font.Font(None, 60)
+finish = False
 
 class GameSprite(pg.sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
@@ -39,28 +41,43 @@ Ball = GameSprite('Ball.png', 200, 250, 5, 50, 50)
 rocketL = Update('rocket.png', 10, 10, 5, 3, 130)
 rocketR = Update('rocket.png', 785, 360, 5, 3, 130)
 
-y_speed = 3
-x_speed = 3
+y_speed = 4
+x_speed = 4
+you_loseL = font1.render('LEFT LOSE!', True, (200, 0, 0))
+you_loseR = font1.render('RIGHT LOSE!', True, (200, 0, 0))
 
 FPS = 60
 Clock = pg.time.Clock()
 while game:
-    window.fill(bg)
     for q in pg.event.get():
         if q.type == pg.QUIT:
             game = False
-    Ball.rect.x += x_speed
-    Ball.rect.y -= y_speed
-    if Ball.rect.y >= 455 or Ball.rect.y <= 0:
-        y_speed *= -1
-    if pg.sprite.collide_rect(Ball, rocketL):
-        x_speed *= -1
-    if pg.sprite.collide_rect(Ball, rocketR):
-        x_speed *= -1
-    rocketL.update_l()
-    rocketR.update_r()
-    rocketL.Outline()
-    rocketR.Outline()
-    Ball.reset()
+    if finish != True:
+        window.fill(bg)
+        Ball.rect.x += x_speed
+        Ball.rect.y -= y_speed
+        if Ball.rect.y >= 455 or Ball.rect.y <= 0:
+            y_speed *= -1
+        if pg.sprite.collide_rect(Ball, rocketL):
+            x_speed *= -1
+            bounces_count += 1
+        if pg.sprite.collide_rect(Ball, rocketR):
+            x_speed *= -1
+            bounces_count += 1
+        if bounces_count == 3:
+            bounces_count = 0
+            y_speed += 1
+            x_speed += 1
+        if Ball.rect.x <= 0:
+            finish = True
+            window.blit(you_loseL, (15, 225))
+        if Ball.rect.x >= 750:
+            finish = True
+            window.blit(you_loseR, (515, 225))
+        rocketL.update_l()
+        rocketR.update_r()
+        rocketL.Outline()
+        rocketR.Outline()
+        Ball.reset()
     pg.display.update()
     Clock.tick(FPS)
